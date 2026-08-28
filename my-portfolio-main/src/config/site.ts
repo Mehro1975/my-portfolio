39d60599f984da88f3ba9a5d1ab388bf5b937ac2
Harsh <okadasan2018@gmail.com>
2026-08-28 21:36:27 +0530

    Update resume link with drive_link parameter

--- my-portfolio-main/src/config/site.ts
+++ my-portfolio-main/src/config/site.ts
@@ -84,7 +84,7 @@ export const site = {
     twitter: "https://x.com/hkj22534690",
     linkedin: "https://linkedin.com/in/harsh-kumar-jha-1b976131b",
     email: "mailto:hjha5316@gmail.com",
-    resume: "https://drive.google.com/file/d/1bOBqtSbU91-pQKmUJRg-M0longH1tA0W/view?usp=sharing",
+    resume: "https://drive.google.com/file/d/1KGyCCn2FDTN8Rr0dyxY8NJ16cw7NzbUV/view?usp=drive_link",
     discord: "",
     medium: "",
   },
